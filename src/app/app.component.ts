38cb8010db5b4bd855eebec8ab034086cedf74bb
Nitai Charan <nitaicharan@gmail.com>
2020-03-09 00:47:43 -0300

    scan, of
    https://rxjs.dev/api/operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'study';
+export class AppComponent implements OnInit {
+  source = of(1, 2, 3);
+  private observable = Observable.create();
+
+  ngOnInit(): void {
+  }
+
+  clicked(event: Event) {
+    this.source.pipe(
+      scan((acc, curr) => acc * curr)
+    ).subscribe(console.log);
+  }
 }
