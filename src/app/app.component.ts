3b231d0209730fe71b69b9b39d7fa45c69a95dbd
Nitai Charan <nitaicharan@gmail.com>
2021-01-17 02:31:52 -0300

    shareReplay
    https://www.learnrxjs.io/learn-rxjs/operators/multicasting/sharereplay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { scan, filter } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { filter, scan } from 'rxjs/operators';
+import { shareReplay, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +14,23 @@ export class AppComponent implements OnInit {
 
     source.pipe(scan(console.log)).subscribe();
 
-
-    source.pipe(scan(console.log)).subscribe();
-
     source.pipe(filter(item => {
       console.log(`Item: ${item} - item !== 2 ${item !== 2}`);
       return item !== 2;
     })).subscribe();
+
+    // grab url and share with subscribers
+    const lastSouce = this.source.pipe(
+      tap(_ => console.log('executed')),
+      shareReplay(1)
+    );
+
+    // requires initial subscription
+    const initialSubscriber = lastSouce.subscribe(console.log);
+
+
+    // logged: 'my-path'
+    const lateSubscriber = lastSouce.subscribe(console.log);
   }
+
 }
